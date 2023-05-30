0c0f8dea0b8d97cec6341f71bbe2ef2812680864
Saurav Sah <saurav.sah@mavs.uta.edu>
2023-05-30 00:42:58 -0500

    feedback ui added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,6 +9,12 @@ import { useNavigate } from "react-router-dom";
 const NavBar = () => {
   const auth = getAuth(app);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [feedBackModal, setFeedBackModal] = useState(false);
+
+  const feedBackHandler = () => {
+    console.log("Feedback clicked");
+    setFeedBackModal((prevState) => !prevState);
+  };
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
@@ -24,28 +30,57 @@ const NavBar = () => {
   }, []);
 
   return (
-    <nav className="NavBar">
-      <div className="navbar-item">AskMe.com</div>
-      <ul className="navbar-item">
-        <li>Feedback</li>
-        {isLoggedIn ? (
-          <li className="userIcon">
-            <FaUser />
-            <span className="hint-text">{auth.currentUser.displayName}</span>
-          </li>
-        ) : (
-          <li className="userIcon"> Login</li>
-        )}
-        {isLoggedIn ? (
-          <li className="userIcon">
-            <FiLogOut />
-            <span className="hint-text">logout</span>
-          </li>
-        ) : (
-          <li className="userIcon">SignUp</li>
-        )}
-      </ul>
-    </nav>
+    <>
+      <nav className="NavBar">
+        <div className="navbar-item">AskMe.com</div>
+        <ul className="navbar-item">
+          <li onClick={feedBackHandler}>Feedback</li>
+          {isLoggedIn ? (
+            <li className="userIcon">
+              <FaUser />
+              <span className="hint-text">{auth.currentUser.displayName}</span>
+            </li>
+          ) : (
+            <li className="userIcon"> Login</li>
+          )}
+          {isLoggedIn ? (
+            <li className="userIcon">
+              <FiLogOut />
+              <span className="hint-text">logout</span>
+            </li>
+          ) : (
+            <li className="userIcon">SignUp</li>
+          )}
+        </ul>
+      </nav>
+      {feedBackModal && <div className="modal-overlay"></div>}
+      {feedBackModal && (
+        <div className="feedbackModal" onClick={feedBackHandler}>
+          <div className="feedbackModal-content">
+            <h2>Feedback Form </h2>
+            <div className="modal-input-div">
+              <input
+                className="modal-input-control"
+                type="text"
+                placeholder="Name"
+              />
+              <input
+                className="modal-input-control"
+                type="email"
+                placeholder="Email"
+              />
+              <textarea
+                className="modal-input-control-text-area"
+                id=""
+                cols="30"
+              ></textarea>
+
+              <button className="submit-form-button"> Submit Form</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
